e94e91fca04688b3e553bf5c29bac9471130def2
Stef Visser <s.visser6200@gmail.com>
2025-06-21 22:29:02 +0200

    Changed vite to also serve VPS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,14 +18,14 @@ export default defineConfig({
     },
   },
   server: {
-    allowedHosts: ['localhost'],
+    allowedHosts: ['localhost', 'springyield.stefvisser.com'],
     host: '0.0.0.0',        // Listen on all network interfaces
     port: 5173,            // Specify the port for the dev server
-    strictPort: true,      // Do not fall back to another port if 20300 is in use
+    strictPort: true,      // Do not fall back to another port if 5173 is in use
     cors: true,             // Enable CORS for all origins
-    // HMR settings removed for local development, Vite will handle it.
     hmr: {
-      clientPort: 20300
+      clientPort: 20300,
+      host: 'auto'
     },
     watch: {
       usePolling: true
